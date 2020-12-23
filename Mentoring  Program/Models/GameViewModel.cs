5561c34b7681df668f4c1a951076932ba3cc1456
Oleksii Venherets <U371770@danfoss.com>
2020-12-23 11:20:14 +0200

    homework 5

--- Mentoring  Program/Models/GameViewModel.cs
+++ Mentoring  Program/Models/GameViewModel.cs
@@ -5,25 +5,15 @@ using System.Threading.Tasks;
 
 namespace MentoringProgram.Models
 {
-    public class Game
+    public class GameViewModel
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public DateTime? ReleaseDate { get; set; }
 
-        public List<string> Genres { get; set; }
-
-        public bool IsFreeToPlay { get; set; }
-
-        public decimal? Price { get; set; }
-
         public string Description { get; set; }
-
-        public string SystemRequirenments { get; set; }
-
-        public List<string> InterfaceLanguages { get; set; }
-
-        public List<string> FullSupportLanguage { get; set; }
 
         public string Developer { get; set; }
 
--- Mentoring  Program/Program.cs
+++ Mentoring  Program/Program.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Mentoring__Program
+namespace MentoringProgram
 {
     public class Program
     {
@@ -13,11 +18,6 @@ namespace Mentoring__Program
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
